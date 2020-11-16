67865d9ce88a8eace1120912c371bb999cdff040
Andrei Ripanu <andrei.ripanu93@gmail.com>
2020-11-16 10:05:47 +0200

    StudentRepository

--- src/Repository/StudentRepository.php
+++ src/Repository/StudentRepository.php
@@ -8,6 +8,10 @@ use Doctrine\Persistence\ManagerRegistry;
 
 class StudentRepository extends ServiceEntityRepository
 {
+  /**
+   * @param ManagerRegistry $registry
+   * @param string|null $entityClass
+   */
   public function __construct(ManagerRegistry $registry, ?string $entityClass = null)
   {
     parent::__construct($registry, Student::class);

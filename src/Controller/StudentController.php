112b5ac7f93b31c20a0395351c65e733270f6f46
Andrei Ripanu <andrei.ripanu93@gmail.com>
2020-11-16 10:42:36 +0200

    student controller

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -8,13 +8,17 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
- * Class StudentController.
+ * Here are all API ENDPOINTS for student resource.
+ * This API is a RESTful API because it allow us to get, create, update and delete objects
+ * with the HTTP verbs GET, POST, PUT / PATCH and DELETE.
+ * Content type is JSON for both (client and server). So, Content-Type header must be application/json.
  *
- * @Route("/api/student", name="api_student")
+ * @Route("/api/v1.0")
  */
 class StudentController extends AbstractController
 {
@@ -24,29 +28,41 @@ class StudentController extends AbstractController
   private EntityManagerInterface $em;
 
   /**
-   * StudentController constructor.
-   *
-   * @param EntityManagerInterface $em
+   * @var TranslatorInterface
    */
-  public function __construct(EntityManagerInterface $em)
+  private TranslatorInterface $translator;
+
+
+  /**
+   * @param EntityManagerInterface $em
+   * @param TranslatorInterface $translator
+   */
+  public function __construct(EntityManagerInterface $em, TranslatorInterface $translator)
   {
     $this->em = $em;
+    $this->translator = $translator;
   }
 
   /**
    * @param StudentRepository $studentRepository
+   * @param Request $request
    * @return JsonResponse
    *
-   * @Route("/all", name="api_student_all", methods={"GET"})
+   * @Route("/students", name="api_students_list", methods={"GET"})
    */
-  public function all(StudentRepository $studentRepository): JsonResponse
+  public function list(StudentRepository $studentRepository, Request $request): JsonResponse
   {
+    $response = $this->checkJsonRequest($request);
+
+    if(!empty($response)) {
+      return $this->json($response, $response['statusCode']);
+    }
+
     $response = [
-      'statusCode' => 200,
+      'statusCode' => Response::HTTP_OK,
       'message' => [],
       'data' => []
     ];
-
     $students = $studentRepository->findBy([], ['id' => 'DESC']);
 
     foreach ($students as $s) {
@@ -59,26 +75,32 @@ class StudentController extends AbstractController
   /**
    * @param int $id
    * @param StudentRepository $studentRepository
+   * @param Request $request
    * @return JsonResponse
    *
-   * @Route("/{id}/show", name="api_student_show", methods={"GET"})
+   * @Route("/students/{id}", name="api_students_show", methods={"GET"})
    */
-  public function show(int $id, StudentRepository $studentRepository): JsonResponse
+  public function show(int $id, StudentRepository $studentRepository, Request $request): JsonResponse
   {
+    $response = $this->checkJsonRequest($request);
+
+    if(!empty($response)) {
+      return $this->json($response, $response['statusCode']);
+    }
+
     $student = $studentRepository->find($id);
 
     if(empty($student)) {
       $response = [
-        'statusCode' => 404,
-        'message' => ['Student not found'],
-        'data' => []
+        'statusCode' => Response::HTTP_NOT_FOUND,
+        'message' => [$this->translator->trans('message.not_found', ['name' => 'Student'])]
       ];
 
       return $this->json($response, $response['statusCode']);
     }
 
     $response = [
-      'statusCode' => 200,
+      'statusCode' => Response::HTTP_OK,
       'message' => [],
       'data' => $student->objectToArray(),
     ];
@@ -87,10 +109,19 @@ class StudentController extends AbstractController
   }
 
   /**
-   * @Route("/create", name="api_student_create", methods={"POST"})
+   * @param Request $request
+   * @return JsonResponse
+   *
+   * @Route("/students", name="api_students_create", methods={"POST"})
    */
-  public function create(Request $request, TranslatorInterface $translator)
+  public function create(Request $request)
   {
+    $response = $this->checkJsonRequest($request);
+
+    if(!empty($response)) {
+      return $this->json($response, $response['statusCode']);
+    }
+
     $data = \json_decode($request->getContent(), true);
     $form = $this->createForm(StudentType::class);
 
@@ -98,33 +129,39 @@ class StudentController extends AbstractController
       $form->submit($data);
     } catch (\Exception $e) {}
 
-    if($form->isSubmitted() && $form->isValid()) {
-      $student = $form->getData();
-      $em = $this->getDoctrine()->getManager();
-      $em->persist($student);
-      $em->flush();
-
-      $response = [
-        'statusCode' => 201,
-        'message' => ['Student created successfully'],
-        'data' => $student->objectToArray(),
-      ];
-    } else {
+    if(!$form->isSubmitted() || !$form->isValid()) {
       $errors = [];
 
       foreach ($form->getErrors(true) as $error) {
-        $message = $translator->trans($error->getMessage());
+        $message = $this->translator->trans($error->getMessage());
 
         if(!in_array($message, $errors)) {
           $errors[] = $message;
         }
       }
 
+      if(empty($errors)) {
+        $errors[] = 'Data not valid';
+      }
+
       $response = [
-        'statusCode' => 422,
+        'statusCode' => Response::HTTP_UNPROCESSABLE_ENTITY,
         'message' => $errors
       ];
+
+      return $this->json($response, $response['statusCode']);
     }
+
+    $student = $form->getData();
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($student);
+    $em->flush();
+
+    $response = [
+      'statusCode' => Response::HTTP_CREATED,
+      'message' => [$this->translator->trans('message.created', ['name' => 'Student'])],
+      'data' => $student->objectToArray(),
+    ];
 
     return $this->json($response, $response['statusCode']);
   }
@@ -132,18 +169,25 @@ class StudentController extends AbstractController
   /**
    * @param int $id
    * @param StudentRepository $studentRepository
+   * @param Request $request
    * @return JsonResponse
    *
-   * @Route("/{id}/edit", name="api_student_edit", methods={"PUT"})
+   * @Route("/students/{id}", name="api_students_edit", methods={"PUT"})
    */
-  public function edit(int $id, StudentRepository $studentRepository, Request $request, TranslatorInterface $translator)
+  public function edit(int $id, StudentRepository $studentRepository, Request $request)
   {
+    $response = $this->checkJsonRequest($request);
+
+    if(!empty($response)) {
+      return $this->json($response, $response['statusCode']);
+    }
+
     $student = $studentRepository->find($id);
 
     if(empty($student)) {
       $response = [
-        'statusCode' => 404,
-        'message' => ['Student not found']
+        'statusCode' => Response::HTTP_NOT_FOUND,
+        'message' => [$this->translator->trans('message.not_found', ['name' => 'Student'])]
       ];
 
       return $this->json($response, $response['statusCode']);
@@ -156,20 +200,11 @@ class StudentController extends AbstractController
       $form->submit($data);
     } catch (\Exception $e) {}
 
-    if($form->isSubmitted() && $form->isValid()) {
-      $this->em->persist($student);
-      $this->em->flush();
-
-      $response = [
-        'statusCode' => 200,
-        'message' => ['Student updated successfully'],
-        'data' => $student->objectToArray(),
-      ];
-    } else {
+    if(!$form->isSubmitted() || !$form->isValid()) {
       $errors = [];
 
       foreach ($form->getErrors(true) as $error) {
-        $message = $translator->trans($error->getMessage());
+        $message = $this->translator->trans($error->getMessage());
 
         if(!in_array($message, $errors)) {
           $errors[] = $message;
@@ -177,14 +212,25 @@ class StudentController extends AbstractController
       }
 
       if(empty($errors)) {
-        $errors[] = 'Data not valid';
+        $errors[] = $this->translator->trans('invalid.data');
       }
 
       $response = [
-        'statusCode' => 422,
+        'statusCode' => Response::HTTP_UNPROCESSABLE_ENTITY,
         'message' => $errors
       ];
+
+      return $this->json($response, $response['statusCode']);
     }
+
+    $this->em->persist($student);
+    $this->em->flush();
+
+    $response = [
+      'statusCode' => Response::HTTP_OK,
+      'message' => [$this->translator->trans('message.updated', ['name' => 'Student'])],
+      'data' => $student->objectToArray(),
+    ];
 
     return $this->json($response, $response['statusCode']);
   }
@@ -192,17 +238,24 @@ class StudentController extends AbstractController
   /**
    * @param int $id
    * @param StudentRepository $studentRepository
+   * @param Request $request
    * @return JsonResponse
    *
-   * @Route("/{id}/delete", name="api_student_delete", methods={"DELETE"})
+   * @Route("/students/{id}", name="api_students_delete", methods={"DELETE"})
    */
-  public function delete(int $id, StudentRepository $studentRepository) {
+  public function delete(int $id, StudentRepository $studentRepository, Request $request) {
+    $response = $this->checkJsonRequest($request);
+
+    if(!empty($response)) {
+      return $this->json($response, $response['statusCode']);
+    }
+
     $student = $studentRepository->find($id);
 
     if(empty($student)) {
       $response = [
-        'statusCode' => 404,
-        'message' => ['Student not found']
+        'statusCode' => Response::HTTP_NOT_FOUND,
+        'message' => [$this->translator->trans('message.not_found', ['name' => 'Student'])],
       ];
 
       return $this->json($response, $response['statusCode']);
@@ -212,10 +265,40 @@ class StudentController extends AbstractController
     $this->em->flush();
 
     $response = [
-      'statusCode' => 200,
-      'message' => ['Student deleted successfully']
+      'statusCode' => Response::HTTP_OK,
+      'message' => [$this->translator->trans('message.deleted', ['name' => 'Student'])]
     ];
 
     return $this->json($response, $response['statusCode']);
+  }
+
+  /**
+   * @param Request $request
+   * @return array
+   */
+  private function checkJsonRequest(Request $request): array
+  {
+    $response = [];
+
+    if(!empty($request->getContent())) {
+      \json_decode($request->getContent(), true);
+    }
+
+    switch (true) {
+      case $request->getContentType() != 'json':
+        $response = [
+          'statusCode' => Response::HTTP_NOT_ACCEPTABLE,
+          'message' => $this->translator->trans('message.json_content'),
+        ];
+        break;
+      case json_last_error():
+        $response = [
+          'statusCode' => Response::HTTP_BAD_REQUEST,
+          'message' => $this->translator->trans('message.json'),
+        ];
+        break;
+    }
+
+    return $response;
   }
 }
